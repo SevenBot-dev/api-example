9fe307ec88df313f3c08c889b346563b88c53bf0
Nanashi <sevenc_nanashi@yahoo.co.jp>
2021-12-03 17:09:37 +0900

    Add: DELETE/afkを追加

---
+++ DELETE-afk/httprb.rb
@@ -0,0 +1,25 @@
+require "http"
+
+USER_ID = 00000000000000000
+TOKEN = "ThisIsT0ken"
+
+print "通信中…"
+
+def delete_afk()
+  response = HTTP.delete("https://api.sevenbot.jp/afk")
+ 
+  case response.code
+  when 419
+    puts "\nレートリミットに到達しました、やり直しています。"
+    sleep response.headers["Ratelimit-Reset"]
+    delete_afk
+  when 401
+    $stderr.puts "認証に失敗しました。"
+    exit 1
+  when 200
+    return
+  end
+end
+
+delete_afk
+puts "完了"
---
+++ DELETE-afk/requests.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+import time
+
+USER_ID = 00000000000000000
+TOKEN = "ThisIsT0ken"
+
+print("通信中...", end="")
+
+def delete_afk():
+    response = requests.delete(
+        "https://api.sevenbot.jp/afk",
+        headers={
+            "Authorization": f"{USER_ID} {TOKEN}"
+        }
+    )
+    if response.status_code == 401:
+        sys.stderr.write("認証に失敗しました。\n")
+        sys.exit(1)
+    elif response.status_code == 419:
+        print("\nレートリミットに到達しました、やり直しています。")
+        time.sleep(response.headers["Ratelimit-Reset"])
+        delete_afk()
+    else:
+        return
+
+delete_afk()
+print("完了")
