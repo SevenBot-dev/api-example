d92ffd6a734619b53d759fa323df11c4c77b9f8f
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-12-03 18:19:54 +0900

    Add: ツイートなどを表示するように

---
+++ DELETE-afk/py_requests.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import sys
+import time
+
+USER_ID = 00000000000000000
+TOKEN = "ThisIsT0ken"
+
+print("通信中...")
+
+
+def delete_afk():
+    response = requests.delete("https://api.sevenbot.jp/afk", headers={"Authorization": f"{USER_ID} {TOKEN}"})
+    if response.status_code == 401:
+        sys.stderr.write("認証に失敗しました。\n")
+        sys.exit(1)
+    elif response.status_code == 419:
+        print("レートリミットに到達しました、やり直しています。")
+        time.sleep(response.headers["Ratelimit-Reset"])
+        delete_afk()
+    else:
+        response_data = json.loads(response.text)
+        if response_data["code"] == "not_afk":
+            print("AFK状態ではありません。")
+            sys.exit(0)
+        print("AFKを解除しました。")
+        print("メンションされたメッセージのURL：")
+        if not response_data["urls"]:
+            print("  (なし)")
+        else:
+            for url in response_data["urls"]:
+                print(f"- {url}")
+        if response_data["tweet"] is not None:
+            print("ツイートされたメッセージのURL：")
+            if response_data["tweet"] is False:
+                print("  (失敗)")
+            else:
+                print(f"- {response_data['tweet']}")
+        return
+
+
+delete_afk()
---
+++ DELETE-afk/rb_http.rb
@@ -0,0 +1,53 @@
+require "http"
+require "json"
+
+USER_ID = 00000000000000000
+TOKEN = "ThisIsT0ken"
+
+puts "通信中…"
+
+def delete_afk()
+  # @type [HTTP::Response]
+  response = HTTP.delete(
+    "https://api.sevenbot.jp/afk",
+    headers: {
+      "Authorization" => "#{USER_ID} #{TOKEN}",
+    },
+  )
+
+  case response.code
+  when 419
+    puts "レートリミットに到達しました、やり直しています。"
+    sleep response.headers["Ratelimit-Reset"]
+    delete_afk
+  when 401
+    $stderr.puts "認証に失敗しました。"
+    exit 1
+  else
+    response_data = JSON.parse(response.body, symbolize_names: true)
+    if response_data[:code] == "not_afk"
+      puts "AFK状態ではありません。"
+      exit 0
+    end
+    puts "AFKを解除しました。"
+    puts "メンションされたメッセージのURL："
+    if response_data[:urls].empty?
+      puts "  (なし)"
+    else
+      response_data[:urls].each do |url|
+        puts "- " + url
+      end
+    end
+    unless response_data[:tweet].nil?
+      puts "ツイートされたメッセージのURL："
+      if response_data[:tweet] == false
+        puts "  (失敗)"
+      else
+        puts "- " + response_data[:tweet]
+      end
+    end
+    return
+  end
+end
+
+delete_afk
